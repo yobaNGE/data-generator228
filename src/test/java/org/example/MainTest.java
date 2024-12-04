5511a664a1d74e42a24f21c3466a7986a5bcd546
1.kla$ <recum12233@mail.ru>
2024-12-04 15:00:16 +0300

    uh

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,6 +2,7 @@ package org.example;
 
 import com.github.javafaker.Faker;
 import com.mongodb.client.*;
+import com.mongodb.client.model.Indexes;
 import org.bson.BsonDocument;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -26,7 +27,7 @@ public class MainTest {
         mongoClient = MongoClients.create(URI);
         database = mongoClient.getDatabase(DATABASE_NAME);
         collection = database.getCollection(COLLECTION_NAME);
-        System.out.println("Done!");
+        System.out.println("Setup is done!\nBegin testing...");
     }
 
     // Закрытие подключения к базе данных после тестов
@@ -36,34 +37,51 @@ public class MainTest {
     }
 
     private final String URI = "mongodb://localhost:27017";
-    private final String DATABASE_NAME = "mydatabase";
-    private final String COLLECTION_NAME = "books";
-    private final List<String> OWNER_LIST = List.of("Bob", "Mike", "Jake");
+    private final String DATABASE_NAME = "lab2db";
+    private final String COLLECTION_NAME = "review";
+    private final List<String> REVIEWER_LIST = List.of("Лешь15", "Анна89", "Игорь_Индезит", "Витек_Самсунг", "Наталья_Ксяоми");
+    private final List<String> TAGS_LIST = List.of("полезное", "уморительно", "спам", "уныние", "продуктивно", "скука");
     private final Faker faker = new Faker();
     private final JsonWriterSettings jsonSettings = JsonWriterSettings.builder()
             .indent(true)
             .outputMode(JsonMode.SHELL)
             .build();
+
     public <T> T oneOf(List<T> list) {
         return list.get(faker.random().nextInt(list.size()));
     }
 
+    Set<String> tags = new HashSet<>();
+
+    public <T> Set<T> fewOf(List<T> list, int uniqueCount) {
+        Set<T> uniqueValues = new HashSet<>();
+        while (uniqueValues.size() < uniqueCount) {
+            uniqueValues.add(list.get(faker.random().nextInt(list.size())));
+        }
+        return uniqueValues;
+    }
+    @Test
+    public void createIndexes() {
+        collection.createIndex(Indexes.ascending("tags"));
+        System.out.println("Indexes created successfully!");
+    }
     // Метод по генерации данных
     public Document generateData() {
         Map<String, Object> doc = new LinkedHashMap<>();
-        doc.put("title", faker.book().title());
-        doc.put("author", faker.book().author());
-        doc.put("year", faker.date().past(1, TimeUnit.DAYS));
-        doc.put("inStock", faker.bool().bool());
-        doc.put("arr", Arrays.asList(1, 2, 3));
-        doc.put("owner", oneOf(OWNER_LIST));
+        doc.put("username", oneOf(REVIEWER_LIST));
+        doc.put("product", faker.commerce().productName());
+        doc.put("rating", faker.number().numberBetween(1, 6));
+        doc.put("comment", faker.lorem().paragraph(faker.random().nextInt(1, 5)));
+        doc.put("date", faker.date().past(365, TimeUnit.DAYS));
+        doc.put("tags", fewOf(TAGS_LIST, faker.random().nextInt(1, 3)));
+
         return new Document(doc);
     }
 
     // Метод по выводу 5 сгенерированных значений в консоль
     @Test
     public void writeToConsole() {
-        for (int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(generateData().toJson(jsonSettings));
         }
     }
@@ -72,7 +90,7 @@ public class MainTest {
     @Test
     public void writeToDatabase() {
         List<Document> documents = new ArrayList<>();
-        for (int i=0; i<10000; i++) {
+        for (int i = 0; i < 1_000_00; i++) {
             documents.add(generateData());
         }
         collection.insertMany(documents);
@@ -81,7 +99,7 @@ public class MainTest {
     @Test
     public void explainRequest() {
         Document result = collection
-                .find(BsonDocument.parse("{owner: 'Mike'}"))
+                .find(BsonDocument.parse("{username: 'Анна89'}"))
                 .explain();
         System.out.println(result.toJson(jsonSettings));
     }
@@ -100,21 +118,18 @@ public class MainTest {
     public void aggregationRequest() {
         List<Bson> pipeline = new ArrayList<>();
         pipeline.add(
-            BsonDocument.parse("""
-                    {
-                        $bucket:
-                          {
-                            groupBy: '$owner',
-                            boundaries: ['Bob', 'Jake', 'Mike'],
-                            default: 'Others',
-                            output: {
-                              count: {
-                                $sum: 1,
-                              },
-                            },
-                          },
-                      },   
-                    """)
+                BsonDocument.parse("""
+            {
+                $bucket: {
+                    groupBy: '$username',
+                    boundaries: ['Анна89', 'Лешь15'],
+                    default: 'Others',
+                    output: {
+                        count: { $sum: 1 }
+                    }
+                }
+            }
+            """)
         );
         AggregateIterable<Document> results = collection.aggregate(pipeline);
         for (Document result : results) {
